b3c2ae978484a52fe5a89b789498a6643dc7abba
my_git_test <inpyo6262@gmail.com>
2020-04-16 02:34:13 -0700

    hw2-2 done

--- main.cpp
+++ main.cpp
@@ -3,6 +3,15 @@
 int sorted[10];
 
 int main(){
-
+    int nums[9];
+    int i;
+    for(i = 0;i<10;++i)
+        cin >> nums[i];
+    merge_sort(nums, 0, 9);
+    for (i = 0; i<10; ++i)
+    {
+        cout << nums[i];
+        cout << ' ';
+    }
     return 0;
 }
